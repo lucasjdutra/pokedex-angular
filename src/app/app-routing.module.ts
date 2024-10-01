cff04855d606c4a1f57f5e110a143a0fdeb88383
lucasjdutra <lucasjdc90@gmail.com>
2024-10-01 20:27:11 -0300

    feat: pokedex button added to homepage

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,11 @@ const routes: Routes = [
   {
     path: 'home',
     loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule)
+  },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
   }
 ];
 
